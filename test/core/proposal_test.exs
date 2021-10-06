8143b4aeee216b7b69936706643fa32163d66dd1
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-10-05 23:29:14 -0300

    test: implements loan value test

--- test/core/proposal_test.exs
+++ test/core/proposal_test.exs
@@ -20,4 +20,30 @@ defmodule Core.ProposalTest do
       assert Proposal.new(params) == expected_proposal
     end
   end
+
+  describe "valid?/1" do
+    test "loan should be between 30k and 3M" do
+      invalid_low_value_proposal = %Proposal{
+        id: 1,
+        loan_value: 29_999,
+        number_of_monthly_installments: 120
+      }
+
+      invalid_high_value_proposal = %Proposal{
+        id: 1,
+        loan_value: 3_000_001,
+        number_of_monthly_installments: 120
+      }
+
+      valid_proposal = %Proposal{
+        id: 1,
+        loan_value: 30_000,
+        number_of_monthly_installments: 120
+      }
+
+      assert Proposal.valid?(invalid_low_value_proposal) == false
+      assert Proposal.valid?(invalid_high_value_proposal) == false
+      assert Proposal.valid?(valid_proposal) == true
+    end
+  end
 end
