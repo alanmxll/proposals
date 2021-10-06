83f888b4dc8e992315173e4d9c40de143855f951
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-10-05 23:29:30 -0300

    chore: implements is valid loan valid function

--- lib/core/proposal.ex
+++ lib/core/proposal.ex
@@ -4,4 +4,8 @@ defmodule Core.Proposal do
   def new(fields) do
     struct!(__MODULE__, fields)
   end
+
+  def valid?(proposal) do
+    proposal.loan_value >= 30_000 && proposal.loan_value <= 3_000_000
+  end
 end
