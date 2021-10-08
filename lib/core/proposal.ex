3c8e1846beae81498ad9b0aff2e3797a1f214538
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-10-07 21:49:41 -0300

    chore: implements validation has one main proponentt

--- lib/core/proposal.ex
+++ lib/core/proposal.ex
@@ -13,6 +13,7 @@ defmodule Core.Proposal do
     |> evaluate(&valid_loan_value?/1)
     |> evaluate(&valid_number_of_monthly_installments?/1)
     |> evaluate(&has_at_least_two_proponents?/1)
+    |> evaluate(&has_exactly_one_main_proponent?/1)
     |> result()
   end
 
@@ -39,5 +40,13 @@ defmodule Core.Proposal do
 
   defp has_at_least_two_proponents?(proposal) do
     Enum.count(proposal.proponents) >= 2
+  end
+
+  defp has_exactly_one_main_proponent?(proposal) do
+    number_of_main_proponents =
+      proposal.proponents
+      |> Enum.count(&(&1.main == true))
+
+    number_of_main_proponents == 1
   end
 end
