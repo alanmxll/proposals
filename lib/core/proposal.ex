a8ccfcd1046c684132ae5583fdb25b7ef0087f02
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-10-07 21:32:33 -0300

    chore: implements proponent struct and two proponents validation

--- lib/core/proposal.ex
+++ lib/core/proposal.ex
@@ -1,5 +1,5 @@
 defmodule Core.Proposal do
-  defstruct [:id, :loan_value, :number_of_monthly_installments]
+  defstruct [:id, :loan_value, :number_of_monthly_installments, proponents: [], warranties: []]
 
   alias Core.Validator
 
@@ -12,6 +12,7 @@ defmodule Core.Proposal do
     |> to_validator()
     |> evaluate(&valid_loan_value?/1)
     |> evaluate(&valid_number_of_monthly_installments?/1)
+    |> evaluate(&has_at_least_two_proponents?/1)
     |> result()
   end
 
@@ -34,5 +35,9 @@ defmodule Core.Proposal do
   defp valid_number_of_monthly_installments?(proposal) do
     proposal.number_of_monthly_installments >= 24 &&
       proposal.number_of_monthly_installments <= 180
+  end
+
+  defp has_at_least_two_proponents?(proposal) do
+    Enum.count(proposal.proponents) >= 2
   end
 end
