2199d473e60eaf5d73474655a3106fadc09319f9
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-10-09 18:02:05 -0300

    refactor: create a file to proposal fixtures

--- test/core/proposal_test.exs
+++ test/core/proposal_test.exs
@@ -1,58 +1,8 @@
 defmodule Core.ProposalTest do
   use ExUnit.Case, async: true
+  import Fixtures.ProposalFixture
 
   alias Core.{Proponent, Proposal}
-
-  @fields %{id: 1, loan_value: 35_000, number_of_monthly_installments: 120}
-  @proponent_fields %{id: 1, name: "Alan", age: 28, monthly_income: 4_000, main: false}
-  @main_proponent_fields %{id: 1, name: "Alan", age: 28, monthly_income: 4_000, main: true}
-
-  def base_proposal(params \\ %{}) do
-    new_params =
-      params
-      |> Enum.into(@fields)
-
-    Map.merge(%Proposal{}, new_params)
-  end
-
-  def valid_proposal(params \\ %{}) do
-    proposal = base_proposal(params)
-
-    proponents = default_proponents()
-    # TODO: need to add default warranties
-
-    %{proposal | proponents: proponents}
-  end
-
-  def default_proponents() do
-    [
-      %Proponent{name: "Laise", age: 27, monthly_income: 5_000, main: false},
-      %Proponent{name: "Maia", age: 32, monthly_income: 8_000, main: true}
-    ]
-  end
-
-  def proposal_without_proponents(params \\ %{}) do
-    proposal = base_proposal(params)
-    %{proposal | proponents: []}
-  end
-
-  def add_proponent(proposal, params \\ %{}) do
-    new_params =
-      params
-      |> Enum.into(@proponent_fields)
-
-    proponent = Map.merge(%Proponent{}, new_params)
-    %{proposal | proponents: [proponent | proposal.proponents]}
-  end
-
-  def add_main_proponent(proposal, params \\ %{}) do
-    new_params =
-      params
-      |> Enum.into(@main_proponent_fields)
-
-    proponent = Map.merge(%Proponent{}, new_params)
-    %{proposal | proponents: [proponent | proposal.proponents]}
-  end
 
   describe "new/1" do
     test "should return a new proposal when all params are passed" do
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,2 @@
+Code.require_file("fixtures/proposal_fixture.exs", __DIR__)
 ExUnit.start()
